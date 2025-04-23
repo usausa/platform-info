31b39d2be1b391c4172b9fd402f30eeceb1e4861
usausa <machi.pon@gmail.com>
2025-04-23 12:19:44 +0900

    Update

--- Example/ExampleDarwin.cs
+++ Example/ExampleDarwin.cs
@@ -9,5 +9,46 @@ internal static class ExampleDarwin
     {
         var uptime = DarwinPlatform.GetUptimeInfo();
         Console.WriteLine(uptime.Uptime);
+
+        var drives = DriveInfo.GetDrives();
+        foreach (var drive in drives)
+        {
+            if (drive.IsReady && IsTargetDriveType(drive.DriveType))
+            {
+                var totalSize = drive.TotalSize;
+                var freeSpace = drive.TotalFreeSpace;
+                var usedSpace = totalSize - freeSpace;
+
+                var usagePercentage = ((double)usedSpace / totalSize) * 100;
+
+                Console.WriteLine($"Drive: {drive.Name} {drive.VolumeLabel}");
+                Console.WriteLine($"  Type: {drive.DriveType}");
+                Console.WriteLine($"  Format: {drive.DriveFormat}");
+                Console.WriteLine($"  Total Size: {FormatBytes(totalSize)}");
+                Console.WriteLine($"  Used Space: {FormatBytes(usedSpace)}");
+                Console.WriteLine($"  Usage: {usagePercentage:F2}%");
+            }
+        }
+    }
+
+    private static bool IsTargetDriveType(DriveType type) =>
+        type != DriveType.Removable &&
+        type != DriveType.Network &&
+        type != DriveType.CDRom &&
+        type != DriveType.Ram;
+
+    private static string FormatBytes(long bytes)
+    {
+        string[] sizes = ["B", "KB", "MB", "GB", "TB"];
+        var order = 0;
+        double len = bytes;
+
+        while (len >= 1024 && order < sizes.Length - 1)
+        {
+            order++;
+            len /= 1024;
+        }
+
+        return $"{len:F2} {sizes[order]}";
     }
 }
